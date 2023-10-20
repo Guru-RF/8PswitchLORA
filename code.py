4303a1d5754359b2fd3c755ba372619065acdec0
ure <ure@moreorless.be>
2023-10-21 01:48:58 +0200

    fixes

--- code.py
+++ code.py
@@ -7,39 +7,60 @@ import config
 
 print("Intializing µPico")
 
-rf1 = DigitalInOut(board.GP5)
+rf1 = DigitalInOut(board.GP23)
 rf1.direction = Direction.OUTPUT
-
 rf1.value = True
 time.sleep(0.25)
-rf1.value = False
 
-rf2 = DigitalInOut(board.GP6)
+rf2 = DigitalInOut(board.GP22)
 rf2.direction = Direction.OUTPUT
-
 rf2.value = True
 time.sleep(0.25)
-rf2.value = False
 
-rf3 = DigitalInOut(board.GP7)
+rf3 = DigitalInOut(board.GP14)
 rf3.direction = Direction.OUTPUT
-
 rf3.value = True
 time.sleep(0.25)
-rf3.value = False
 
-rf4 = DigitalInOut(board.GP8)
+rf4 = DigitalInOut(board.GP13)
 rf4.direction = Direction.OUTPUT
-
 rf4.value = True
 time.sleep(0.25)
+
+rf5 = DigitalInOut(board.GP0) # 23
+rf5.direction = Direction.OUTPUT
+rf5.value = True
+time.sleep(0.25)
+
+rf6 = DigitalInOut(board.GP1) # 22
+rf6.direction = Direction.OUTPUT
+rf6.value = True
+time.sleep(0.25)
+
+rf7 = DigitalInOut(board.GP2) # 14
+rf7.direction = Direction.OUTPUT
+rf7.value = True
+time.sleep(0.25)
+
+rf8 = DigitalInOut(board.GP3) # 13
+rf8.direction = Direction.OUTPUT
+rf8.value = True
+time.sleep(0.25)
+
+rf8.value = False
+rf7.value = False
+rf6.value = False
+rf5.value = False
 rf4.value = False
+rf3.value = False
+rf2.value = False
+rf1.value = False
 
 # Lora Stuff
 RADIO_FREQ_MHZ = 868.000
 CS = DigitalInOut(board.GP21)
 RESET = DigitalInOut(board.GP20)
-spi = busio.SPI(board.GP18, MOSI=board.GP19, MISO=board.GP16)
+spi = busio.SPI(board.GP10, MOSI=board.GP11, MISO=board.GP8)
 rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, RADIO_FREQ_MHZ, baudrate=1000000, agc=False,crc=True)
 rfm9x.tx_power = 5
 
@@ -59,23 +80,79 @@ while True:
                         rf2.value = False
                         rf3.value = False
                         rf4.value = False
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = False
                     
                     if port is 2:
                         rf1.value = False
                         rf2.value = True
                         rf3.value = False
                         rf4.value = False
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = False
                     
                     if port is 3:
                         rf1.value = False
                         rf2.value = False
                         rf3.value = True
                         rf4.value = False
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = False
                     
                     if port is 4:
                         rf1.value = False
                         rf2.value = False
                         rf3.value = False
                         rf4.value = True
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = False
+
+                    if port is 5:
+                        rf1.value = False
+                        rf2.value = False
+                        rf3.value = False
+                        rf4.value = False
+                        rf5.value = True
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = False
+                    
+                    if port is 6:
+                        rf1.value = False
+                        rf2.value = False
+                        rf3.value = False
+                        rf4.value = False
+                        rf5.value = False
+                        rf6.value = True
+                        rf7.value = False
+                        rf8.value = False
+                    
+                    if port is 7:
+                        rf1.value = False
+                        rf2.value = False
+                        rf3.value = False
+                        rf4.value = False
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = True
+                        rf8.value = False
+                    
+                    if port is 8:
+                        rf1.value = False
+                        rf2.value = False
+                        rf3.value = False
+                        rf4.value = False
+                        rf5.value = False
+                        rf6.value = False
+                        rf7.value = False
+                        rf8.value = True
         else:
             print("Unknown packet")
